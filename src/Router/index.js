f74c1fc32b382a4f862382347c06acc67fda8368
tongenze <364064955@qq.com>
2023-11-24 10:45:32 +0800

    xx

--- src/Router/index.js
+++ src/Router/index.js
@@ -1,7 +1,7 @@
 import { Navigate, useRoutes } from "react-router-dom"
 import React, { lazy } from "react"
 import configureStore from '../Store'
-
+import KeepAlive from 'react-activation'
 import LoginView from "../Page/LoginView"
 import HomeView from "../Page/HomeView"
 import ContentView from "../Page/ContentView"
@@ -68,12 +68,12 @@ export const Router = function () {
             {
               id: 1,
               path: "/home/content/page1",
-              element: RequireAuth(<Page1 />, 1),
+              element: RequireAuth(<KeepAlive cacheKey='1'><Page1 /></KeepAlive>, 1),
             },
             {
               id: 2,
               path: "/home/content/page2",
-              element: RequireAuth(<Page2 />, 2),
+              element: RequireAuth(<KeepAlive cacheKey='2'><Page2 /></KeepAlive>, 2),
             },
             {
               id: 3,
--- src/index.js
+++ src/index.js
@@ -6,17 +6,18 @@ import reportWebVitals from "./reportWebVitals"
 import { BrowserRouter } from "react-router-dom"
 import { Provider } from "react-redux"
 import Store from "./Store"
-
+import { AliveScope } from 'react-activation'
 const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
+  <BrowserRouter>
+    <AliveScope>
       <Provider store={Store}>
         <App />
       </Provider>
-    </BrowserRouter>
-  </React.StrictMode>
+    </AliveScope>
+  </BrowserRouter>
 )
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
